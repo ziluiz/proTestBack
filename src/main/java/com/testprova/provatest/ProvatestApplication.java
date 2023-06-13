90bf9c966d259d308bcad1c519c2bb78a1e35673
ziluiz <luiz021205@gmail.com>
2023-06-13 09:03:03 -0300

    first commit

---
+++ src/main/java/com/testprova/provatest/ProvatestApplication.java
@@ -0,0 +1,13 @@
+package com.testprova.provatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testprova/provatest/ProvatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testprova.provatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
